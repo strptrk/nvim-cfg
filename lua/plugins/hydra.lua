ea772e3acfd9d919f2bb6a96142041aeeef67f09
Patrik Stier <patrik.stier@ericsson.com>
2023-05-04 10:40:29 +0200

    fixed removed options hydra

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -35,8 +35,8 @@ return {
    _K_: prev hunk   _u_: undo last stage   _p_: preview hunk      _C_: commit diffview ^ ^ ^
    ^ ^              _S_: stage buffe       _P_: preview hunk (il) _F_: file history
    ^ ^              _R_: reset hunk        _b_: blame line       
-   ^ ^ ^ ^                                 _B_: blame show full   _;_: Gitsigns   
    ^ ^
+   ^ ^ ^ ^                                 _B_: blame show full   _;_: Gitsigns   
    ^ ^              _<Enter>_: Lazygit              _q_: exit
       ]]
 
@@ -159,6 +159,145 @@ return {
             nil,
             { exit = true, nowait = true, desc = 'exit' },
           },
+        },
+      })
+
+      hint = [[
+      ^ ^        Options ^ ^ ^
+      ^
+   _v_ %{ve} virtual edit
+   _i_ %{list} invisible characters  
+   _s_ %{spell} spell
+   _w_ %{wrap} wrap
+   _l_ %{cul} cursor line
+   _c_ %{cc} color column
+   _n_ %{nu} number
+   _r_ %{rnu} relative number
+      ^
+           _q_ / _<Esc>_
+      ]]
+
+      Hydra({
+        name = 'Options',
+        hint = hint,
+        config = {
+          color = 'amaranth',
+          invoke_on_body = true,
+          hint = {
+            border = 'rounded',
+            position = 'bottom',
+            funcs = {
+              cc = function()
+                if vim.o.colorcolumn ~= '80,120' then
+                  return '[ ]'
+                end
+                return '[x]'
+              end,
+            },
+          },
+        },
+        mode = { 'n', 'x' },
+        body = '<A-o>',
+        heads = {
+          {
+            'n',
+            function()
+              if vim.o.number == true then
+                vim.o.number = false
+              else
+                vim.o.number = true
+              end
+            end,
+            { desc = 'number' },
+          },
+          {
+            'r',
+            function()
+              if vim.o.relativenumber == true then
+                vim.o.relativenumber = false
+              else
+                vim.o.number = true
+                vim.o.relativenumber = true
+              end
+            end,
+            { desc = 'relativenumber' },
+          },
+          {
+            'v',
+            function()
+              if vim.o.virtualedit == 'all' then
+                vim.o.virtualedit = 'block'
+              else
+                vim.o.virtualedit = 'all'
+              end
+            end,
+            { desc = 'virtualedit' },
+          },
+          {
+            'i',
+            function()
+              if vim.o.list == true then
+                vim.o.list = false
+              else
+                vim.o.list = true
+              end
+            end,
+            { desc = 'show invisible' },
+          },
+          {
+            's',
+            function()
+              if vim.o.spell == true then
+                vim.o.spell = false
+              else
+                vim.o.spell = true
+              end
+            end,
+            { exit = true, desc = 'spell' },
+          },
+          {
+            'w',
+            function()
+              if vim.o.wrap ~= true then
+                vim.o.wrap = true
+                vim.keymap.set('n', 'k', function()
+                  return vim.v.count > 0 and 'k' or 'gk'
+                end, { expr = true, desc = 'k or gk' })
+                vim.keymap.set('n', 'j', function()
+                  return vim.v.count > 0 and 'j' or 'gj'
+                end, { expr = true, desc = 'j or gj' })
+              else
+                vim.o.wrap = false
+                vim.keymap.del('n', 'k')
+                vim.keymap.del('n', 'j')
+              end
+            end,
+            { desc = 'wrap' },
+          },
+          {
+            'l',
+            function()
+              if vim.o.cursorline == true then
+                vim.o.cursorline = false
+              else
+                vim.o.cursorline = true
+              end
+            end,
+            { desc = 'cursor line' },
+          },
+          {
+            'c',
+            function()
+              if vim.o.colorcolumn ~= '80,120' then
+                vim.o.colorcolumn = '80,120'
+              else
+                vim.o.colorcolumn = ''
+              end
+            end,
+            { desc = 'color column' },
+          },
+          { '<Esc>', nil, { exit = true } },
+          { 'q', nil, { exit = true } },
         },
       })
 
