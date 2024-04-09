340b225c2caf338f64731d50765307290eb7e6bc
strptrk <git@patrikstier.com>
2024-04-09 15:32:26 +0200

    updated neotree

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v2.x',
+    branch = 'v3.x',
     lazy = true,
     keys = {
       {
@@ -259,7 +259,7 @@ return {
               --".null-ls_*",
             },
           },
-          follow_current_file = true,            -- This will find and focus the file in the active buffer every
+          follow_current_file = { enabled = false },            -- This will find and focus the file in the active buffer every
           -- time the current file is changed while the tree is open.
           group_empty_dirs = false,               -- when true, empty folders will be grouped together
           hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
@@ -287,7 +287,7 @@ return {
           },
         },
         buffers = {
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
+          follow_current_file = { enabled = true }, -- This will find and focus the file in the active buffer every
           -- time the current file is changed while the tree is open.
           group_empty_dirs = true,    -- when true, empty folders will be grouped together
           show_unloaded = true,
