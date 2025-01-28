0c3f18dd602eeb0523f84310760dc07926a568ef
Patrik Stier <git@patrikstier.com>
2025-01-28 22:01:40 +0100

    disable oil.nvim auto preview

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -272,17 +272,17 @@ return {
       { "<A-o>", function() require("oil").toggle_float() end },
       { ",o", function() require("oil").open() end },
     },
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "OilEnter",
-        callback = vim.schedule_wrap(function(args)
-          local oil = require("oil")
-          if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-            oil.open_preview()
-          end
-        end),
-      })
-    end,
+    -- init = function()
+      -- vim.api.nvim_create_autocmd("User", {
+      --   pattern = "OilEnter",
+      --   callback = vim.schedule_wrap(function(args)
+      --     local oil = require("oil")
+      --     if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+      --       oil.open_preview()
+      --     end
+      --   end),
+      -- })
+    -- end,
     opts = {
       watch_for_changes = true,
       keymaps = {
