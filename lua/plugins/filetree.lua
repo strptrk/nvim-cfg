bcb25237bb7d414f399b7aebed38de4aba78307a
strptrk <git@patrikstier.com>
2025-02-10 22:19:21 +0100

    enable Oil auto-preview

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -269,20 +269,21 @@ return {
       "Oil",
     },
     keys = {
-      { "<A-o>", function() require("oil").toggle_float() end },
-      { ",o", function() require("oil").open() end },
+      { "<A-o>", function()
+        if vim.w.is_oil_win then
+          require("oil").close()
+        else
+          require("oil").open_float(nil, {}, function()
+            require("oil").open_preview()
+          end)
+        end
+      end },
+      { ",o", function()
+        require("oil").open(nil, {}, function()
+          require("oil").open_preview()
+        end)
+      end },
     },
-    -- init = function()
-      -- vim.api.nvim_create_autocmd("User", {
-      --   pattern = "OilEnter",
-      --   callback = vim.schedule_wrap(function(args)
-      --     local oil = require("oil")
-      --     if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-      --       oil.open_preview()
-      --     end
-      --   end),
-      -- })
-    -- end,
     opts = {
       watch_for_changes = true,
       keymaps = {
